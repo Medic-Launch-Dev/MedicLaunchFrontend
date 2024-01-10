fc0e9d54fe911d37bf3964247745a8d9c3a8e85e
Abdullah <ahmedabdullah60@gmail.com>
2024-01-09 21:00:30 -0800

    Re-enable protected routes

--- src/services/ProtectedRoute.tsx
+++ src/services/ProtectedRoute.tsx
@@ -1,12 +1,13 @@
 // ProtectedRoute.tsx
 
+import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
 
 export const ProtectedRoute = ({ children }) => {
   const { isLoggedIn } = useAuth();
 
-  // if (!isLoggedIn()) {
-  //   return <Navigate to="/login" />;
-  // }
+  if (!isLoggedIn()) {
+    return <Navigate to="/login" />;
+  }
   return children;
 };
