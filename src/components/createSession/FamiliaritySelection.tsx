963d1b4d9a93883c5e7ae4efb3b2b4c65c9d82b2
Abdullah <ahmedabdullah60@gmail.com>
2024-01-11 09:24:27 -0800

    Filtering UI complete

---
+++ src/components/createSession/FamiliaritySelection.tsx
@@ -0,0 +1,43 @@
+import { Grid } from "@mui/material";
+import { useState } from "react";
+import FilterOption from "./FamiliarityOption";
+
+export default function FamiliaritySelection() {
+  const familiarities = [
+    {
+      heading: "New questions",
+      subheading: "Questions you've never attempted before",
+    },
+    {
+      heading: "Incorrect questions",
+      subheading: "Questions you got incorrect on your last attempt",
+    },
+    {
+      heading: "Flagged questions",
+      subheading: "Questions you flagged in previous sessions",
+    },
+    {
+      heading: "All questions",
+      subheading: "Every question in the UKMLA question bank",
+    },
+  ]
+
+  const [familiarity, setFamiliarity] = useState<string>();
+
+  return (
+    <Grid container spacing={6} height="100%" px={12} py={3}>
+      {
+        familiarities.map(f => (
+          <Grid item xs={6}>
+            <FilterOption
+              heading={f.heading}
+              subheading={f.subheading}
+              selected={familiarity === f.heading}
+              setSelected={setFamiliarity}
+            />
+          </Grid>
+        ))
+      }
+    </Grid>
+  )
+}
