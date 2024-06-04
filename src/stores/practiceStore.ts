2f9f02d27366b9987650fa81f488769d6db458f0
Abdullah <a@randmar.io>
2024-06-04 12:16:06 +0400

    Add get stats route

--- src/stores/practiceStore.ts
+++ src/stores/practiceStore.ts
@@ -3,74 +3,78 @@ import { Familiarity, PracticeFilter, QuestionsOrder } from "../models/PracticeF
 import MedicLaunchApiClient from "../services/MedicLaunchApiClient";
 
 export enum TimerState {
-    STOPPED,
-    RUNNING,
-    PAUSED,
+	STOPPED,
+	RUNNING,
+	PAUSED,
 }
 
 export class PracticeStore {
-    private apiClient: MedicLaunchApiClient;
-    practiceFilter: PracticeFilter;
-    examTimerState: TimerState = TimerState.STOPPED;
-    timeRemaining: number = 7;
-    showEndMessage: boolean = false;
-    private timerInterval: NodeJS.Timeout;
+	private apiClient: MedicLaunchApiClient;
+	practiceFilter: PracticeFilter;
+	examTimerState: TimerState = TimerState.STOPPED;
+	timeRemaining: number = 7;
+	showEndMessage: boolean = false;
+	private timerInterval: NodeJS.Timeout;
 
-    constructor(apiClient: MedicLaunchApiClient) {
-        this.practiceFilter = new PracticeFilter()
-        this.apiClient = apiClient;
-        makeAutoObservable(this);
-    }
+	constructor(apiClient: MedicLaunchApiClient) {
+		this.practiceFilter = new PracticeFilter()
+		this.apiClient = apiClient;
+		makeAutoObservable(this);
+	}
 
-    startTimer() {
-        this.examTimerState = TimerState.RUNNING;
-        this.timerInterval = setInterval(() => {
-            if (this.timeRemaining > 0) {
-                this.timeRemaining--;
-            } else {
-                this.timeRemaining = 0;
-                this.pauseTimer();
-                this.showEndMessage = true;
-            }
-        }, 1000);
-    }
+	startTimer() {
+		this.examTimerState = TimerState.RUNNING;
+		this.timerInterval = setInterval(() => {
+			if (this.timeRemaining > 0) {
+				this.timeRemaining--;
+			} else {
+				this.timeRemaining = 0;
+				this.pauseTimer();
+				this.showEndMessage = true;
+			}
+		}, 1000);
+	}
 
-    pauseTimer() {
-        this.examTimerState = TimerState.PAUSED;
-        clearInterval(this.timerInterval);
-    }
+	pauseTimer() {
+		this.examTimerState = TimerState.PAUSED;
+		clearInterval(this.timerInterval);
+	}
 
-    resetTimer() {
-        this.examTimerState = TimerState.STOPPED;
-        this.timeRemaining = 7;
-        clearInterval(this.timerInterval);
-    }
+	resetTimer() {
+		this.examTimerState = TimerState.STOPPED;
+		this.timeRemaining = 7;
+		clearInterval(this.timerInterval);
+	}
 
-    setShowEndMessage(showEndMessage: boolean) {
-        this.showEndMessage = showEndMessage;
-    }
+	setShowEndMessage(showEndMessage: boolean) {
+		this.showEndMessage = showEndMessage;
+	}
 
-    setSelectedSpecialities(specialities: string[]) {
-        this.practiceFilter.specialityIds = specialities;
-    }
+	setSelectedSpecialities(specialities: string[]) {
+		this.practiceFilter.specialityIds = specialities;
+	}
 
-    setFamiliarity(familiarity: Familiarity) {
-        this.practiceFilter.familiarity = familiarity;
-    }
+	setFamiliarity(familiarity: Familiarity) {
+		this.practiceFilter.familiarity = familiarity;
+	}
 
-    setQuestionsOrder(questionsOrder: QuestionsOrder) {
-        this.practiceFilter.selectionOrder = questionsOrder;
-    }
+	setQuestionsOrder(questionsOrder: QuestionsOrder) {
+		this.practiceFilter.selectionOrder = questionsOrder;
+	}
 
-    setQuestionsCount(questionsCount: number) {
-        this.practiceFilter.questionsCount = questionsCount;
-    }
+	setQuestionsCount(questionsCount: number) {
+		this.practiceFilter.questionsCount = questionsCount;
+	}
 
-    setAllSpecialitiesSelected(allSpecialitiesSelected: boolean) {
-        this.practiceFilter.allSpecialitiesSelected = allSpecialitiesSelected;
-    }
+	setAllSpecialitiesSelected(allSpecialitiesSelected: boolean) {
+		this.practiceFilter.allSpecialitiesSelected = allSpecialitiesSelected;
+	}
 
-    async getPracticeQuestions() {
-        return await this.apiClient.filterQuestions(this.practiceFilter);
-    }
+	async getPracticeQuestions() {
+		return await this.apiClient.filterQuestions(this.practiceFilter);
+	}
+
+	async getPracticeStats() {
+		return await this.apiClient.getData(`practice/practicestats`);
+	}
 }
